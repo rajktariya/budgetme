5325f614050b06f79427cf972ea58d3ac219a60d
rajeshsaini <rajesh.saini@simpplr.com>
2021-06-20 13:52:29 +0530

    add expnse

---
+++ app/src/main/java/com/example/budgetme/Addexpense.java
@@ -0,0 +1,21 @@
+package com.example.budgetme;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Addexpense extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.add_expense);
+
+
+
+    }
+}
--- app/src/main/java/com/example/budgetme/Home.java
+++ app/src/main/java/com/example/budgetme/Home.java
@@ -1,6 +1,10 @@
 package com.example.budgetme;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +15,16 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.home_screen);
+
+        Button addexpense  = findViewById(R.id.addexpense);
+
+        addexpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this,Addexpense.class));
+                Toast.makeText(Home.this,"Add expnse button fdgsg",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
